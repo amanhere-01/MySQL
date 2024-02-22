db48df9c3e2f5e3ccfbe4a485c1ccb25c6a7a7ad
amanhere-01 <amanhere0101@gmail.com>
2024-02-22 11:59:38 +0530

    DB1

---
+++ DB1.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE players;
+USE players;
+CREATE TABLE player(
+	id INT 	PRIMARY KEY,
+    name VARCHAR(50)
+);
+INSERT INTO player VALUES(101, 'ChiconyPlays');
+INSERT INTO player (id, name) VALUES (301, 'NoobAtMission');	
+	
+SELECT * FROM player;
+DESC player;
